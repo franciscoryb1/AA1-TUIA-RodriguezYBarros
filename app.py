c99a57015cfbf1a6ec6cb482a26a08551eb32c20
franciscoryb1 <fraciscoryb1@hotmail.com>
2024-08-04 01:16:55 -0300

    streamlit app

--- app.py
+++ app.py
@@ -1,143 +1,130 @@
-# Librerias
-from sklearn.pipeline import Pipeline
-from sklearn.preprocessing import StandardScaler
-from sklearn.base import BaseEstimator, TransformerMixin
-from sklearn.linear_model import LinearRegression, LogisticRegression
 import pandas as pd
 import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.linear_model import (
+    LinearRegression,
+    Ridge,
+    Lasso,
+    ElasticNet,
+    RidgeCV,
+    ElasticNetCV,
+    LassoCV,
+    SGDRegressor,
+    LogisticRegression
+)
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import (
+     mean_squared_error, 
+     r2_score, 
+     mean_absolute_error,
+     classification_report, 
+     confusion_matrix,
+     ConfusionMatrixDisplay,
+     balanced_accuracy_score, 
+     log_loss,
+     roc_curve, 
+     roc_auc_score, 
+     auc,
+     accuracy_score
+)
+import shap
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import f1_score
+from tensorflow.keras.callbacks import EarlyStopping
+import numpy as np
+import tensorflow as tf
+from sklearn.metrics import f1_score as sklearn_f1_score
+import pandas as pd
+import numpy as np
+import tensorflow as tf
+import optuna
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+from sklearn.utils.class_weight import compute_class_weight
+import tensorflow as tf
+import pandas as pd
+import numpy as np
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
+from sklearn.base import BaseEstimator, TransformerMixin, ClassifierMixin, RegressorMixin
 import joblib
 
-# Carga de dataset y limpieza inicial
-file_path = "weatherAUS.csv"
-df0 = pd.read_csv(file_path, sep=",", engine="python")
-df = df0.copy()
-ciudades = [
-    " Adelaide",
-    "Canberra",
-    "Cobar",
-    "Dartmoor",
-    "Melbourne",
-    "MelbourneAirport",
-    "MountGambier",
-    "Sydney",
-    "SydneyAirport",
-]  
-# Filtrar por ciudades
-df = df[df["Location"].isin(ciudades)]
-df = df.drop("Location", axis=1)
-# X
-X = df.drop(['RainTomorrow', 'RainfallTomorrow', 'Unnamed: 0'], axis=1)
-# y
-# y = df[["RainfallTomorrow"]]
-y = df[["RainTomorrow"]]
-mode = y['RainTomorrow'].mode()[0]
-y = y['RainTomorrow'].fillna(mode)
-X = X.drop(['WindGustDir', 'WindDir9am', 'WindDir3pm', 'RainToday'], axis=1)
-
-# TransformData
-class TransformData20(BaseEstimator, TransformerMixin):
+class CustomScaler(BaseEstimator, TransformerMixin):
     def __init__(self):
-        pass
-        
-    def fit(self, X, y):
-        return self  # No se necesita hacer ningún ajuste en el fit para este caso
-    
-    def transform(self, X, y=None):        
-        # Eliminar columna "Unnamed: 0"
-        if 'Unnamed: 0' in X.columns:
-            X = X.drop("Unnamed: 0", axis=1)
-        
-        # Convertir la fecha a datetime
-        X['Date'] = pd.to_datetime(X['Date'])
-        
-        # Determinar bimestre
-        X['Bimestre'] = X['Date'].apply(self.determinar_bimestre)
-        
-        # Rellenar valores faltantes de Rainfall
-        mediana_por_dia = X.groupby(X["Date"].dt.date)["Rainfall"].median()
-        X["Rainfall"] = X.apply(
-            lambda row: mediana_por_dia[row["Date"].date()] if pd.isnull(row["Rainfall"]) else row["Rainfall"],
-            axis=1,
-        )
-        
-        # Rellenar valores faltantes de Evaporation por bimestre
-        medianas_evaporation = X.groupby("Bimestre")["Evaporation"].median()
-        for bimestre, median in medianas_evaporation.items():
-            X.loc[(X["Bimestre"] == bimestre) & (X["Evaporation"].isnull()), "Evaporation"] = median
-        
-        # Rellenar valores faltantes de Sunshine por día
-        X['Sunshine'] = X.groupby(X['Date'].dt.day)["Sunshine"].transform(lambda x: x.fillna(x.mean()))
+        self.scaler = StandardScaler()
+        self.columnas_numericas = None
+        self.columnas_categoricas = ['WindGustDir', 'WindDir9am', 'WindDir3pm', 'RainToday']
 
-        # Rellenar valores faltantes de WindDir por día
-        # X["WindGustDir"] = X.groupby(X["Date"].dt.day)[
-        #     "WindGustDir"
-        # ].transform(lambda x: x.fillna(x.mode().iloc[0]))
-        # X["WindDir9am"] = X.groupby(X["Date"].dt.day)[
-        #     "WindDir9am"
-        # ].transform(lambda x: x.fillna(x.mode().iloc[0]))
-        # X["WindDir3pm"] = X.groupby(X["Date"].dt.day)[
-        #     "WindDir3pm"
-        # ].transform(lambda x: x.fillna(x.mode().iloc[0]))
-        
-        # Rellenar valores faltantes de WindSpeed, Humidity, Cloud, Pressure, Temp por día
-        columns_to_fillna = ['WindGustSpeed', 'WindSpeed9am', 'WindSpeed3pm', 'Humidity9am', 'Humidity3pm', 'Cloud9am',
-                             'Cloud3pm', 'Pressure9am', 'Pressure3pm', 'MinTemp', 'MaxTemp', 'Temp9am', 'Temp3pm']
-        
-        for column in columns_to_fillna:
-            if column in X.columns:
-                X[column] = X.groupby(X['Date'].dt.day)[column].transform(lambda x: x.fillna(x.median()))
-        
-        # Rellenar valores faltantes de RainToday con la moda y pasarlo a 1 y 0
-        # moda_RainToday = X.groupby("Date")["RainToday"].transform(
-        #     lambda x: x.mode().iloc[0] if not x.mode().empty else None
-        # )
-        # X["RainToday"] = X["RainToday"].fillna(moda_RainToday)
-        # X["RainToday"] = X["RainToday"].map({"Yes": 1, "No": 0})
+    def fit(self, X, y=None):
+        if 'Date' in X.columns:
+            self.columnas_categoricas.append('Date')
+        self.columnas_numericas = X.columns.difference(self.columnas_categoricas)
+        X_numeric = X[self.columnas_numericas]
+        self.scaler.fit(X_numeric)
+        return self
 
-        # Agrupar direcciones de viento
-        # X['WindGustDir'] = X['WindGustDir'].apply(self.agrupar_direcciones)
-        # X['WindDir9am'] = X['WindDir9am'].apply(self.agrupar_direcciones)
-        # X['WindDir3pm'] = X['WindDir3pm'].apply(self.agrupar_direcciones)
+    def transform(self, X, y=None):
+        X_numeric = X[self.columnas_numericas]
+        X_escalado_numeric = self.scaler.transform(X_numeric)
+        X_esc = pd.DataFrame(X_escalado_numeric, columns=self.columnas_numericas, index=X.index)
+        X_scaled = X[self.columnas_categoricas].join(X_esc)
+        return X_scaled
 
-        # Crear variables dummies para direcciones agrupadas
-        # X = pd.get_dummies(X, columns=['WindGustDir', 'WindDir9am', 'WindDir3pm'],
-        #                     drop_first=True)
-        # X.replace({True: 1, False: 0}, inplace=True)
+class DummiesTransformer(BaseEstimator, TransformerMixin):
+    def __init__(self):
+        self.columns_ = None
+
+    def fit(self, X, y=None):
+        X = X.copy()
+
+        # Agrupar direcciones y convertir a dummies
+        X["RainToday"] = X["RainToday"].map({"Yes": 1, "No": 0}).astype(float)
+        X["WindGustDir_Agrupado"] = X["WindGustDir"].apply(self.agrupar_direcciones)
+        X["WindDir9am_Agrupado"] = X["WindDir9am"].apply(self.agrupar_direcciones)
+        X["WindDir3pm_Agrupado"] = X["WindDir3pm"].apply(self.agrupar_direcciones)
+
+        # Convertir a dummies
+        X = self.convertir_a_dummies(X, "WindGustDir_Agrupado", "WindGustDir")
+        X = self.convertir_a_dummies(X, "WindDir9am_Agrupado", "WindDir9am")
+        X = self.convertir_a_dummies(X, "WindDir3pm_Agrupado", "WindDir3pm")
+
+        # Eliminar las columnas originales
+        X = X.drop(["WindGustDir", "WindDir9am", "WindDir3pm"], axis=1)
+
+        # Guardar las columnas generadas durante el fit
+        self.columns_ = X.columns
         
-        # Crear variables dummies para direcciones agrupadas
-        # dummies = pd.get_dummies(X, columns=['WindGustDir', 'WindDir9am', 'WindDir3pm'], drop_first=True)
-        # dummies = dummies[['WindGustDir_N', 'WindGustDir_S', 'WindGustDir_W', 'WindDir9am_N', 'WindDir9am_S', 'WindDir9am_W', 'WindDir3pm_N', 'WindDir3pm_S', 'WindDir3pm_W']]
-        # X = pd.concat([X, dummies], axis=1)
+        return self
+
+    def transform(self, X, y=None):
+        X = X.copy()
+
+        # Aplicar las mismas transformaciones que en fit
+        X["RainToday"] = X["RainToday"].map({"Yes": 1, "No": 0}).astype(float)
+        X["WindGustDir_Agrupado"] = X["WindGustDir"].apply(self.agrupar_direcciones)
+        X["WindDir9am_Agrupado"] = X["WindDir9am"].apply(self.agrupar_direcciones)
+        X["WindDir3pm_Agrupado"] = X["WindDir3pm"].apply(self.agrupar_direcciones)
+
+        # Convertir a dummies
+        X = self.convertir_a_dummies(X, "WindGustDir_Agrupado", "WindGustDir")
+        X = self.convertir_a_dummies(X, "WindDir9am_Agrupado", "WindDir9am")
+        X = self.convertir_a_dummies(X, "WindDir3pm_Agrupado", "WindDir3pm")
+
+        # Eliminar las columnas originales
+        X = X.drop(["WindGustDir", "WindDir9am", "WindDir3pm"], axis=1)
         
-        # Calcular diferencia de temperatura máxima y mínima
-        # X['Dif_Temp_Max_Min'] = X['MaxTemp'] - X['MinTemp']
-        # X = X.drop(['MaxTemp', 'MinTemp'], axis=1)
+        # Asegurarse de que todas las columnas de fit están presentes en la transformación
+        for col in self.columns_:
+            if col not in X.columns:
+                X[col] = 0
         
-        # Calcular diferencia de temperaturas 9am y 3pm
-        # X['Temp_Difference'] = X['Temp3pm'] - X['Temp9am']
-        # X = X.drop(['Temp3pm', 'Temp9am'], axis=1)
-        
-        # Eliminar columnas innecesarias
-        X = X.drop(['Date', 'Bimestre'], axis=1)
-        # X = X.drop(['Bimestre'], axis=1)
+        # Reordenar las columnas como en el fit
+        X = X[self.columns_]
         
         return X
-    
-    def determinar_bimestre(self, fecha):
-        mes = fecha.month
-        if 1 <= mes <= 2:
-            return "Bimestre 1"
-        elif 3 <= mes <= 4:
-            return "Bimestre 2"
-        elif 5 <= mes <= 6:
-            return "Bimestre 3"
-        elif 7 <= mes <= 8:
-            return "Bimestre 4"
-        elif 9 <= mes <= 10:
-            return "Bimestre 5"
-        else:
-            return "Bimestre 6"
-    
+
     def agrupar_direcciones(self, direccion):
         grupos_principales = {
             "N": ["N", "NNW", "NNE"],
@@ -145,31 +132,107 @@ class TransformData20(BaseEstimator, TransformerMixin):
             "E": ["E", "ENE", "ESE", "SE", "NE"],
             "W": ["W", "WNW", "WSW", "SW", "NW"],
         }
+
         for grupo, direcciones in grupos_principales.items():
             if direccion in direcciones:
                 return grupo
+
         return "Otro"
 
-# Pipeline
-pipeline20 = Pipeline([
-    ('transform_data', TransformData20()),
-    ('regression', LogisticRegression(random_state=42, class_weight='balanced'))
-])
+    def convertir_a_dummies(self, X, columna_agrupada, prefijo):
+        dummies = pd.get_dummies(X[columna_agrupada], dtype=int, drop_first=True)
+        dummies = dummies.rename(columns={
+            "N": f"{prefijo}_N", 
+            "S": f"{prefijo}_S", 
+            "W": f"{prefijo}_W"
+        })
+        X = X.drop(columna_agrupada, axis=1)
+        X = pd.concat([X, dummies], axis=1)
+        return X
 
-# Entrenar el pipeline
-pipeline20.fit(X, y)
-joblib.dump(pipeline20, 'pipeline.joblib')
+from sklearn.utils.class_weight import compute_class_weight
 
-# # Fila para PREDICT
-# pd.set_option('future.no_silent_downcasting', True)
-# # Filtrar el DataFrame
-# fila = df.iloc[10]
-# # Convertir la fila en un nuevo DataFrame
-# df_fila = pd.DataFrame(fila).transpose()
-# df_fila = df_fila.drop(['RainTomorrow', 'RainfallTomorrow', 'Unnamed: 0'], axis=1)
-# transformer = TransformData20()
-# transformer.transform(df_fila)
+class NeuralNetworkTensorFlowRl(BaseEstimator, ClassifierMixin):
+    def __init__(self, batch_size=32, epochs=10, learning_rate=0.001, dropout_rate=0.3, n_units_layer_0=64, n_units_layer_1=32, class_weight=None):
+        self.batch_size = batch_size
+        self.epochs = epochs
+        self.learning_rate = learning_rate
+        self.dropout_rate = dropout_rate
+        self.n_units_layer_0 = n_units_layer_0
+        self.n_units_layer_1 = n_units_layer_1
+        self.class_weight = class_weight
+        self.model = None
 
-# # Predecir con el pipeline
-# predictions = pipeline.predict(df_fila)
-# print(predictions)
+    def build_model(self, input_shape):
+        model = tf.keras.Sequential()
+        model.add(tf.keras.Input(shape=(input_shape,)))
+        model.add(tf.keras.layers.Dense(self.n_units_layer_0, activation='relu'))
+        model.add(tf.keras.layers.Dropout(self.dropout_rate))
+        model.add(tf.keras.layers.Dense(self.n_units_layer_1, activation='relu'))
+        model.add(tf.keras.layers.Dropout(self.dropout_rate))
+        model.add(tf.keras.layers.Dense(1, activation='sigmoid'))
+        model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=self.learning_rate), 
+                      loss='binary_crossentropy', 
+                      metrics=[tf.keras.metrics.Precision(), tf.keras.metrics.Recall()])
+        return model
+
+
+    def fit(self, X, y, X_val=None, y_val=None):
+        X = np.array(X)
+        y = np.array(y).ravel()  # Asegurarse de que y es un array 1D
+
+        self.model = self.build_model(X.shape[1])
+
+        # Calculando los pesos de las clases si no están proporcionados
+        if self.class_weight is None:
+            classes = np.unique(y)  # Obtener las clases únicas
+            class_weights = compute_class_weight(class_weight='balanced', classes=classes, y=y)
+            # Ajustar manualmente el peso de la clase minoritaria
+            class_weights[1] = class_weights[1] * 2  # Doblar el peso de la clase 1, puedes ajustar este valor
+            self.class_weight = dict(enumerate(class_weights))
+
+        # Ajustando el modelo con class_weight
+        self.model.fit(X, y, epochs=self.epochs, batch_size=self.batch_size, verbose=0, class_weight=self.class_weight)
+        return self
+
+    def predict(self, X):
+        X = np.array(X)
+        predictions = self.model.predict(X)
+        return (predictions > 0.5).astype(int)
+
+    def predict_proba(self, X):
+        X = np.array(X)
+        return self.model.predict(X)
+
+class NeuralNetworkTensorFlowRegressor(BaseEstimator, RegressorMixin):
+    def __init__(self, batch_size=32, epochs=10, learning_rate=0.001, dropout_rate=0.3, n_units_layer_0=30):
+        self.batch_size = batch_size
+        self.epochs = epochs
+        self.learning_rate = learning_rate
+        self.dropout_rate = dropout_rate
+        self.n_units_layer_0 = n_units_layer_0
+        self.model = None
+
+    def build_model(self, input_shape):
+        model = Sequential()
+        model.add(Dense(self.n_units_layer_0, activation='relu', input_shape=(input_shape,)))
+        model.add(Dense(1, activation='linear'))
+        model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=self.learning_rate), 
+                      loss='mean_squared_error', 
+                      metrics=['mse'])
+        return model
+
+    def fit(self, X, y, X_val=None, y_val=None):
+        X = np.array(X)
+        y = np.array(y).ravel()  # Asegurarse de que y es un array 1D
+
+        self.model = self.build_model(X.shape[1])
+
+        # Ajustando el modelo sin class_weight ya que es regresión
+        self.model.fit(X, y, epochs=self.epochs, batch_size=self.batch_size, verbose=1)
+        return self
+
+    def predict(self, X):
+        X = np.array(X)
+        predictions = self.model.predict(X)
+        return predictions.flatten()
--- main.py
+++ main.py
@@ -1,57 +1,79 @@
 import streamlit as st
-import joblib
 import pandas as pd
+import joblib
 from datetime import date
-from app import TransformData20
+from app import CustomScaler, DummiesTransformer, NeuralNetworkTensorFlowRl, NeuralNetworkTensorFlowRegressor
+
+# Título de la aplicación
+st.title('Predicción de Lluvia')
+
+# Ruta del modelo entrenado
+pipelineRainTomorrow = './pipelines/pipelineRL.joblib'
+pipelineRainFallTomorrow = './pipelines/pipelineRegressor.joblib'
+modelRainTomorrow = joblib.load(pipelineRainTomorrow)
+modelRainFallTomorrow = joblib.load(pipelineRainFallTomorrow)
 
 # Cargar el modelo entrenado
-st.title('Predicción de Lluvia')
-pipelineRainTomorrow = 'rain_tomorrow.joblib'
-pipelineRainFallTomorrow = 'rain_fall_tomorrow.joblib'
+# try:
+#     modelRainTomorrow = joblib.load(pipelineRainTomorrow)
+#     st.sidebar.success("Pipeline cargado exitosamente.")
+# except FileNotFoundError:
+#     st.sidebar.error(f'Ocurrió un error al cargar el modelo.')
+#     st.stop()  # Detener la ejecución si el modelo no se carga
+# except Exception as ex:
+#     st.sidebar.error(f'Ocurrió un error al cargar el modelo: {ex}')
+#     st.stop()
 
-try:
-    modelRainTomorrow = joblib.load(pipelineRainTomorrow)
-    modelRainFallTomorrow = joblib.load(pipelineRainFallTomorrow)
-    # st.sidebar.success("Pipeline cargado exitosamente.")
-except FileNotFoundError:
-    st.sidebar.error(f'Ocurrió un error al cargar el modelo.')
-except Exception as ex:
-    st.sidebar.error(f'Ocurrió un error al cargar el modelo.')
-
-
-# Sidebar para información adicional o ajustes
+# Sidebar para ajustes
 st.sidebar.header('Ajustes')
 
 # Obtener la fecha actual
 fecha = date.today()
+
 # Sliders para los datos de entrada
 st.sidebar.subheader('Datos Meteorológicos')
+
 minTemp = st.sidebar.slider('Temperatura mínima (°C)', -10.0, 40.0, 20.0)
 maxTemp = st.sidebar.slider('Temperatura máxima (°C)', -10.0, 40.0, 20.0)
-temp9am = st.sidebar.slider('Temperatura 9am (°C)', -10.0, 40.0, 20.0)
-temp3pm = st.sidebar.slider('Temperatura 3pm (°C)', -10.0, 40.0, 20.0)
 rainfall = st.sidebar.slider('Lluvia (mm)', 0.0, 150.0, 20.0)
 evaporation = st.sidebar.slider('Evaporación (mm)', 0.0, 100.0, 50.0)
 sunshine = st.sidebar.slider('Horas de sol', 0.0, 20.0, 10.0)
+WindGustDir = st.sidebar.selectbox(
+    'WindGustDir',
+    options=["N", "NNW", "NNE", "S", "SSW", "SSE", "E", "ENE", "ESE", "SE", "NE", "W", "WNW", "WSW", "SW", "NW"])
 windGustSpeed = st.sidebar.slider('Velocidad de ráfaga de viento (km/h)', 0.0, 130.0, 50.0)
+WindDir9am = st.sidebar.selectbox(
+    'WindDir9am',
+    options=["N", "NNW", "NNE", "S", "SSW", "SSE", "E", "ENE", "ESE", "SE", "NE", "W", "WNW", "WSW", "SW", "NW"])
+WindDir3pm = st.sidebar.selectbox(
+    'WindDir3pm',
+    options=["N", "NNW", "NNE", "S", "SSW", "SSE", "E", "ENE", "ESE", "SE", "NE", "W", "WNW", "WSW", "SW", "NW"])
 windSpeed9am = st.sidebar.slider('Velocidad de viento 9am (km/h)', 0.0, 130.0, 50.0)
 windSpeed3pm = st.sidebar.slider('Velocidad de viento 3pm (km/h)', 0.0, 130.0, 50.0)
 humidity9am = st.sidebar.slider('Humedad 9am (%)', 0.0, 100.0, 50.0)
 humidity3pm = st.sidebar.slider('Humedad 3pm (%)', 0.0, 100.0, 50.0)
-pressure9am = st.sidebar.slider('Presión 9am (hPa)', 0.0, 100.0, 50.0)
-pressure3pm = st.sidebar.slider('Presión 3pm (hPa)', 0.0, 100.0, 50.0)
+pressure9am = st.sidebar.slider('Presión 9am (hPa)', 950.0, 1050.0, 1010.0)
+pressure3pm = st.sidebar.slider('Presión 3pm (hPa)', 950.0, 1050.0, 1010.0)
 cloud9am = st.sidebar.slider('Nubosidad 9am (%)', 0.0, 100.0, 50.0)
 cloud3pm = st.sidebar.slider('Nubosidad 3pm (%)', 0.0, 100.0, 50.0)
+temp9am = st.sidebar.slider('Temperatura 9am (°C)', -10.0, 40.0, 20.0)
+temp3pm = st.sidebar.slider('Temperatura 3pm (°C)', -10.0, 40.0, 20.0)
+RainToday = st.sidebar.selectbox(
+    'RainToday',
+    options=["Si", "No"]
+)
 
 # Crear el DataFrame con los datos ingresados por el usuario
 data = {
-    'Date': [fecha],
     'MinTemp': [minTemp],
     'MaxTemp': [maxTemp],
     'Rainfall': [rainfall],
     'Evaporation': [evaporation],
     'Sunshine': [sunshine],
+    'WindGustDir': [WindGustDir],
     'WindGustSpeed': [windGustSpeed],
+    'WindDir9am': [WindDir9am],
+    'WindDir3pm': [WindDir3pm],
     'WindSpeed9am': [windSpeed9am],
     'WindSpeed3pm': [windSpeed3pm],
     'Humidity9am': [humidity9am],
@@ -62,7 +84,7 @@ data = {
     'Cloud3pm': [cloud3pm],
     'Temp9am': [temp9am],
     'Temp3pm': [temp3pm],
-    'Bimestre': ['Bimestre 1'],  # Opción fija por ahora
+    'RainToday': [RainToday]
 }
 
 # Mostrar los datos ingresados por el usuario
@@ -70,18 +92,17 @@ st.subheader('Datos de Entrada')
 fila = pd.DataFrame(data)
 st.write(fila)
 
-# RainFallTomorrow
+# Hacer la predicción
 prediccion_rain_tomorrow = modelRainTomorrow.predict(fila)
 prediccion_rain_fall_tomorrow = modelRainFallTomorrow.predict(fila)
 
-st.subheader('Predicción RainFallTomorrow')
-st.write(prediccion_rain_fall_tomorrow)
-print(prediccion_rain_tomorrow)
-# RainTomorrow
+# Mostrar la predicción
 st.subheader('Predicción RainTomorrow')
-if prediccion_rain_tomorrow[0] == 'Yes':
+st.write(prediccion_rain_fall_tomorrow)
+st.write(prediccion_rain_tomorrow)
+if prediccion_rain_tomorrow[0] == 1:
     st.write("¡Sí! Probablemente lloverá mañana.")
-    st.image("assets/rainy_icon.png.png", width=100)
+    # st.image("assets/rainy_icon.png", width=100)
 else:
     st.write("No, es probable que no llueva mañana.")
-    st.image("assets/sunny_icon.png", width=100)
+    # st.image("assets/sunny_icon.png", width=100)
